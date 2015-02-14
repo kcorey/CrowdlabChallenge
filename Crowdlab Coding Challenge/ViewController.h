16e8600a845cefbe5af9d49dad46df3d51844cf4
kcorey <ken@kencorey.com>
2015-02-14 09:50:32 +0000

    Initial Commit

---
+++ Crowdlab Coding Challenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Crowdlab Coding Challenge
+//
+//  Created by Ken Corey on 14/02/2015.
+//  Copyright (c) 2015 Flippin' Bits Software, Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Crowdlab Coding Challenge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Crowdlab Coding Challenge
+//
+//  Created by Ken Corey on 14/02/2015.
+//  Copyright (c) 2015 Flippin' Bits Software, Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
